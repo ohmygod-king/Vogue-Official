38f92415cdaf679f7133c5bf845ba65da899a584
quiethub4 <Madindun>
2026-06-10 08:12:28 +0700

    update

--- src/Database/session.js
+++ src/Database/session.js
@@ -3,7 +3,7 @@
 const Database = require('better-sqlite3');
 const path     = require('path');
 const fs       = require('fs');
-const logger   = require('../src/Utils/logger');
+const logger   = require('../Utils/logger');
 const DB_DIR  = path.resolve(__dirname);
 const DB_PATH = path.join(DB_DIR, 'vogue.db');
 
