f82ab471bd7906217ecedefe35d31454c71d99b8
quiethub4 <Madindun>
2026-06-10 08:15:06 +0700

    update logger.js

--- src/Utils/logger.js
+++ src/Utils/logger.js
@@ -1,56 +1,18 @@
+'use strict';
 
-const winston = require('winston');
-const chalk   = require('chalk');
+const pino     = require('pino');
 const settings = require('../Settings');
 
-const { combine, timestamp, printf } = winston.format;
-
-const colorLevel = (level) => {
-  const map = {
-    error: chalk.bold.red,
-    warn:  chalk.bold.yellow,
-    info:  chalk.bold.cyan,
-    debug: chalk.bold.gray,
-  };
-  const fn = map[level] || chalk.white;
-  return fn(level.toUpperCase().padEnd(5));
-};
-
-const devFormat = combine(
-  timestamp({ format: 'HH:mm:ss' }),
-  printf(({ level, message, timestamp, ...meta }) => {
-    const metaStr = Object.keys(meta).length
-      ? chalk.gray(` » ${JSON.stringify(meta)}`)
-      : '';
-    return `${chalk.gray(timestamp)} ${colorLevel(level)} ${message}${metaStr}`;
-  })
-);
-
-const prodFormat = combine(
-  timestamp(),
-  winston.format.json()
-);
-
-const logger = winston.createLogger({
-  level: settings.logLevel,
-  transports: [
-    new winston.transports.Console({
-      format: devFormat,
-    }),
-    new winston.transports.File({
-      filename: 'logs/error.log',
-      level: 'error',
-      maxsize: 5 * 1024 * 1024,
-      maxFiles: 3,
-      format: prodFormat,
-    }),
-    new winston.transports.File({
-      filename: 'logs/app.log',
-      maxsize: 10 * 1024 * 1024,
-      maxFiles: 5,
-      format: prodFormat,
-    }),
-  ],
+const logger = pino({
+  level: settings.isDev ? 'debug' : 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize:      true,
+      translateTime: 'SYS:HH:MM:ss',
+      ignore:        'pid,hostname',
+    },
+  },
 });
 
-module.exports = { logger };
+module.exports = logger;
